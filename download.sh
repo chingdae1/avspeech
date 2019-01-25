6021adb403edfbfa493e4d53b7c8360e13efcef5
chingdae1 <chingdae0820@hanmail.net>
2019-01-25 14:51:23 +0900

    sh

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVS_2 --fourcc mp4v --start 3000 --end 6000 > ./log/log_2 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVS_4 --fourcc mp4v --start 9000 --end 12000 > ./log/log_4 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVS_1 --fourcc mp4v --start 0 --end 3000 > ./log/log_1 &
+
