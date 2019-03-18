a5a0b3ffaf1113b3accae7dbd8ea87175e5aa5e4
chingdae1 <chingdae0820@hanmail.net>
2019-03-18 13:13:29 +0900

    sh

--- download.sh
+++ download.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVS_1 --fourcc mp4v --start 0 --end 3000 > ./log/log_1 &
-
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_1 --fourcc mp4v --start 0 --end 30000 > ./log/log_1 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_2 --fourcc mp4v --start 30000 --end 60000 > ./log/log_2 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_3 --fourcc mp4v --start 60000 --end 90000 > ./log/log_3 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_4 --fourcc mp4v --start 90000 --end 120000 > ./log/log_4 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_5 --fourcc mp4v --start 120000 --end 150000 > ./log/log_5 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStest_6 --fourcc mp4v --start 150000 --end 180000 > ./log/log_6 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_1 --fourcc mp4v --start 180000 --end 210000 > ./log/log_7 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_2 --fourcc mp4v --start 210000 --end 240000 > ./log/log_8 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_3 --fourcc mp4v --start 240000 --end 270000 > ./log/log_9 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_4 --fourcc mp4v --start 270000 --end 300000 > ./log/log_10 &
