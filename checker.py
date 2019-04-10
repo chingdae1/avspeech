c3b5639287098205448947c92c363b93b5f583db
chingdae1 <chingdae0820@hanmail.net>
2019-04-10 15:44:41 +0900

    sh

--- checker.py
+++ checker.py
@@ -6,11 +6,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--AVS_dir', default='/workspace2')
 args = parser.parse_args()
 
-all_AVS = glob.glob(os.path.join(args.AVS_dir, 'AVStrain*'))
+all_AVS = sorted(glob.glob(os.path.join(args.AVS_dir, 'AVStrain*')))
 
 for i, avs in enumerate(all_AVS):
     all_npy = glob.glob(os.path.join(avs, 'numpy', 'audio', '*'))
     all_cropped = glob.glob(os.path.join(avs, 'original', 'cropped', '*'))
 
     is_same = (len(all_npy) ==len(all_cropped))
-    print('npy:', len(all_npy), 'cropped:', len(all_cropped), is_same)
+    dir_name = os.path.dirname(avs)
+    print('in:', dir_name, 'npy:', len(all_npy), 'cropped:', len(all_cropped), is_same)
--- download.sh
+++ download.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ## Download only train dataset. [!] Threre's only train csv in csv directory.
 # For 221 server
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_1 --fourcc mp4v --start 140000 --end 142000 > ./log/log_1 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_2 --fourcc mp4v --start 142000 --end 144000 > ./log/log_2 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_3 --fourcc mp4v --start 144000 --end 146000 > ./log/log_3 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_4 --fourcc mp4v --start 146000 --end 148000 > ./log/log_4 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_5 --fourcc mp4v --start 148000 --end 150000 > ./log/log_5 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_6 --fourcc mp4v --start 150000 --end 152000 > ./log/log_6 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_7 --fourcc mp4v --start 152000 --end 154000 > ./log/log_7 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_8 --fourcc mp4v --start 154000 --end 156000 > ./log/log_8 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_9 --fourcc mp4v --start 156000 --end 158000 > ./log/log_9 &
-nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_10 --fourcc mp4v --start 158000 --end 1600000 > ./log/log_10 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_1 --fourcc mp4v --start 160200 --end 162000 > ./log/log_1 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_2 --fourcc mp4v --start 162000 --end 164000 > ./log/log_2 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_3 --fourcc mp4v --start 164000 --end 166000 > ./log/log_3 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_4 --fourcc mp4v --start 166000 --end 168000 > ./log/log_4 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_5 --fourcc mp4v --start 168000 --end 170000 > ./log/log_5 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd1/AVStrain_6 --fourcc mp4v --start 170000 --end 172000 > ./log/log_6 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_7 --fourcc mp4v --start 172000 --end 174000 > ./log/log_7 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_8 --fourcc mp4v --start 174000 --end 176000 > ./log/log_8 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_9 --fourcc mp4v --start 176000 --end 178000 > ./log/log_9 &
+nohup python3 main.py --csv_dir ./csv --result_dir /ssd2/AVStrain_10 --fourcc mp4v --start 178000 --end 180000 > ./log/log_10 &
